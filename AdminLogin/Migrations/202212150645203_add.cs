05b704dab397e36e8068a88a794c38136ca15f7e
chaitalis <chaitalis@chaitali>
2022-12-15 08:59:39 +0000

    Add project files.

---
+++ AdminLogin/Migrations/202212150645203_add.cs
@@ -0,0 +1,27 @@
+﻿namespace AdminLogin.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class add : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Admins",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        AdminId = c.Int(nullable: false),
+                        Password = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Admins");
+        }
+    }
+}
---
+++ AdminLogin/Models/AdminContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace AdminLogin.Models
+{
+    public class AdminContext : DbContext
+    {
+        public DbSet<Admin> Admins { get; set; }    
+    }
+}
